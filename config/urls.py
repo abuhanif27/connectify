7a764e9e6f0796ebd770a2eb00334f85b827df5b
Abu Hanif <ahanif2500@gmail.com>
2026-09-05 17:27:42 +0600

    feat(conversations): update URL configuration to include conversations app

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('apps.authentication.urls')),
+    path(
+        "conversations/",
+        include("apps.conversations.urls"),
+    ),
 ]
